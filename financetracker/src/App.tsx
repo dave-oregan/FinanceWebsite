f412b77e8987352061c74203163ddde995197ee4
David O'Regan <oreagn10@gmail.com>
2024-06-16 00:58:41 -0400

    Day 3+4
    - Lekton numbers in report
    - Lekton font file
    - fixed overflow
    - sorted out colours
    - reordered categories
    - protected against empty salary
    - reworked number filter
    - added ability to toggle percent for savings and investments
    - reworked some css

--- financetracker/src/App.tsx
+++ financetracker/src/App.tsx
@@ -9,11 +9,12 @@ import DisplayBox from './components/DisplayBox'
 import NewBox from './components/NewBox'
 import PopUp from './components/PopUp'
 
-const COLOURS = [ '#C34A3C', '#CF6D62', '#DB938B', '#8D3CC3', '#B17AD6', '#3CB5C3', '#7ACDD6', '#72C33C', '#C53A93', '#4062BF', '#FF8B12', '#FBCC2E', '#3BC43C', '#CA35A2', '#4C6AD0', '#ED821C', '#FBC804' ]
+const COLOURS = [ '#C34A3C', '#CF6D62', '#DB938B', '#8D3CC3', '#B17AD6', '#3CB5C3', '#7ACDD6', '#E57D10', '#FF8B12', '#FFA241', '#FFB970', '#FFD0A0', '#004080', '#326699', '#DAA520', '#FBCC2E', '#B05A87' ]
 
 var counter = 0
+var amount = true
 
-const initialCategories = ['National Tax', 'Province/State Tax', 'Local Tax', '401K Contribution', 'IRA Contribution', 'Health Insurance', 'Social Security', 'Housing', 'Transport', 'Utility', 'Food', 'Toiletry', 'Internet', 'Phone', 'Free Spending', 'Investment', 'Savings', 'Extra Money']
+const initialCategories = ['National Tax', 'Province/State Tax', 'Local Tax', '401K Contribution', 'IRA Contribution', 'Health Insurance', 'Social Security', 'Housing', 'Transport', 'Utility', 'Food', 'Toiletry', 'Internet', 'Phone', 'Savings', 'Investment', 'Free Spending', 'Extra Money']
 const initialData = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 const App: React.FC = () => {
@@ -40,24 +41,35 @@ const App: React.FC = () => {
     // Iterate through input boxes
     Array.from(document.getElementsByClassName('Input') as HTMLCollectionOf<HTMLInputElement>).forEach(element => {
       console.log(element)
-      if (element.id !== 'salary') { // filter out salary category
+      if (element.id !== 'salary' && newSalary[0] > 0) { // filter out salary category
         if (element.id.includes('_tax')) { // taxes
           if (element.id !== 'deduction_tax') { // filter out deductions
             var tax = ((+element.value)/100)*(newSalary[0]-deduction401k-deductionTax)
             if (deduction401k <= 0 && deductionIRA > 0) {
               tax = ((+element.value)/100)*(newSalary[0]-(deductionIRA*fedTaxRate)-deductionTax)
             }
-            newData.push(tax)
+            newData.push(+tax.toFixed(2))
             newSalary[1] -= tax
           }
         }
         else if (element.id === 'socsecurity') { // social security
           var socsec = ((+element.value)/100)*newSalary[0]
-          newData.push(socsec)
+          newData.push(+socsec.toFixed(2))
           newSalary[1] -= socsec
         }
+        else if (element.id === 'savings' || element.id === 'invest') { // Savings and Investments
+          if (amount) {
+            newData.push(+parseFloat(element.value).toFixed(2))
+            newSalary[1] -= (+element.value)
+          }
+          else {
+            const percent = (+(element.value)/100)*newSalary[0]
+            newData.push(+percent.toFixed(2))
+            newSalary[1] -= (+element.value)
+          }
+        }
         else { // normal inputs
-          newData.push(+element.value)
+          newData.push(+parseFloat(element.value).toFixed(2))
           newSalary[1] -= (+element.value)
         }
       }
@@ -67,8 +79,8 @@ const App: React.FC = () => {
     })
 
     // Display extra money
-    newData.push(newSalary[1])
-    newColours.push('#32A852')
+    newData.push(+newSalary[1].toFixed(2))
+    newColours.push('#6AA84F')
 
     console.log(categories)
 
@@ -123,7 +135,7 @@ const App: React.FC = () => {
               <span>${input}</span>
               <span id='delete${counter}' class='deleteBTN' onclick='this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)'>X</span>
             </div>
-            <input id='miscbox${counter}' class='Input' type='number' step='.0001' onfocus='this.addEventListener("wheel", function (e) { e.preventDefault() }, { passive: false })'></input>`
+            <input id='miscbox${counter}' class='Input' onchange='this.value = /^\\d*\\.?\\d*$/.test(this.value) ? this.value : (numerise(this.value.replace(/[^0-9.]/g, '')))' />`
           generatedbox.addEventListener('change', () => { updateData() })
         container.appendChild(generatedbox);
         // Updates display on delete
@@ -144,6 +156,23 @@ const App: React.FC = () => {
     // Adds event listener to update report dynamically
     Array.from(document.getElementsByClassName('Input') as HTMLCollectionOf<HTMLInputElement>).forEach(element => {
       element.addEventListener('change', () => { updateData() })
+    });
+
+    // Toggles savings and investments as percents
+    (document.getElementById('percent_yn') as HTMLSelectElement).addEventListener('change', () => {
+      const value = (document.getElementById('percent_yn') as HTMLSelectElement).value
+      const sav_inv = document.querySelectorAll('.sav_inv') as NodeListOf<HTMLElement>
+      sav_inv.forEach((input: HTMLElement) => {
+        if (value === 'Amount') {
+          (input.lastChild as HTMLInputElement).classList.remove('percent')
+          amount = true
+          updateData()
+        } else {
+          (input.lastChild as HTMLInputElement).classList.add('percent')
+          amount = false
+          updateData()
+        }
+      })
     })
   }, [])
 
@@ -152,30 +181,31 @@ const App: React.FC = () => {
     <div className='Background'>
       <div className='CentrePanel'>
         <Header />
-        <EntryBox id='salary' parent_class='' label='Please Enter Monthly Income:' type='number' />
+        <EntryBox id='salary' parent_class='' label='Please Enter Monthly Income:' />
         <SubHeader text='Tax' />
         <SelectBox id='tax_yn' label='Factor in Tax?' options={['Yes', 'No']} />
-        <EntryBox id='nation_tax' parent_class='taxboxes' label='Please Enter National Tax:' type='number' />
-        <EntryBox id='province_tax' parent_class='taxboxes' label='Please Enter Province/State Tax:' type='number' />
-        <EntryBox id='local_tax' parent_class='taxboxes' label='Please Enter Local Tax:' type='number' />
-        <EntryBox id='deduction_tax' parent_class='taxboxes' label='Please Enter Total Tax Deduction:' type='number' />
+        <EntryBox id='nation_tax' parent_class='taxboxes' label='Please Enter National Tax:' />
+        <EntryBox id='province_tax' parent_class='taxboxes' label='Please Enter Province/State Tax:' />
+        <EntryBox id='local_tax' parent_class='taxboxes' label='Please Enter Local Tax:' />
+        <EntryBox id='deduction_tax' parent_class='taxboxes' label='Please Enter Total Tax Deduction:' />
         <SubHeader text='Retirement & Health' />
-        <EntryBox id='retirement' parent_class='' label='Please Enter 401K Contribution:' type='number' />
-        <EntryBox id='retirement_ira' parent_class='' label='Please Enter IRA Contribution:' type='number' />
-        <EntryBox id='health' parent_class='' label='Please Enter Health Insurance Cost:' type='number' />
-        <EntryBox id='socsecurity' parent_class='' label='Please Enter Social Security Contribution:' type='number' />
+        <EntryBox id='retirement' parent_class='' label='Please Enter 401K Contribution:' />
+        <EntryBox id='retirement_ira' parent_class='' label='Please Enter IRA Contribution:' />
+        <EntryBox id='health' parent_class='' label='Please Enter Health Insurance Cost:' />
+        <EntryBox id='socsecurity' parent_class='' label='Please Enter Social Security Contribution:' />
         <SubHeader text='Basic Spending' />
-        <EntryBox id='housing' parent_class='' label='Please Enter Monthly Housing Cost:' type='number' />
-        <EntryBox id='car' parent_class='' label='Please Enter Monthly Transport Cost:' type='number' />
-        <EntryBox id='utility' parent_class='' label='Please Enter Monthly Utility Cost:' type='number' />
-        <EntryBox id='food' parent_class='' label='Please Enter Monthly Food Cost:' type='number' />
-        <EntryBox id='toiletries' parent_class='' label='Please Enter Monthly Toiletry Cost:' type='number' />
-        <EntryBox id='internet' parent_class='' label='Please Enter Monthly Internet Cost:' type='number' />
-        <EntryBox id='phone' parent_class='' label='Please Enter Monthly Phone Cost:' type='number' />
+        <EntryBox id='housing' parent_class='' label='Please Enter Monthly Housing Cost:' />
+        <EntryBox id='car' parent_class='' label='Please Enter Monthly Transport Cost:' />
+        <EntryBox id='utility' parent_class='' label='Please Enter Monthly Utility Cost:' />
+        <EntryBox id='food' parent_class='' label='Please Enter Monthly Food Cost:' />
+        <EntryBox id='toiletries' parent_class='' label='Please Enter Monthly Toiletry Cost:' />
+        <EntryBox id='internet' parent_class='' label='Please Enter Monthly Internet Cost:' />
+        <EntryBox id='phone' parent_class='' label='Please Enter Monthly Phone Cost:' />
         <SubHeader text='Free Spending, Investments, & Savings' />
-        <EntryBox id='spending' parent_class='' label='Please Enter Ideal Monthly Free Spending:' type='number' /> 
-        <EntryBox id='invest' parent_class='' label='Please Enter Ideal Monthly Investments:' type='number' /> {/* Make it available as percent also */}
-        <EntryBox id='savings' parent_class='' label='Please Enter Ideal Monthly Savings:' type='number' /> {/* Make it available as percent also */}
+        <SelectBox id='percent_yn' label='Savings and Investments as an Amount or a Percent?' options={['Amount', 'Percent']} />
+        <EntryBox id='savings' parent_class='sav_inv' label='Please Enter Ideal Monthly Savings:' />
+        <EntryBox id='invest' parent_class='sav_inv' label='Please Enter Ideal Monthly Investments:' />
+        <EntryBox id='spending' parent_class='' label='Please Enter Ideal Monthly Free Spending:' /> 
         <SubHeader text='Additional Costs' />
         <div id='MiscHolder'>
         {/* NewBox will add elements here */}
--- financetracker/src/components/EntryBox.tsx
+++ financetracker/src/components/EntryBox.tsx
@@ -5,19 +5,26 @@ interface EntryBoxProps {
     id: string
     parent_class: string
     label: string
-    type: string
 }
 
-const EntryBox: React.FC<EntryBoxProps> = ({id, parent_class, label, type}) => {
+// Additional handling for removing non-numbers from inputs
+const numerise = (target: string) => {
+    const parts = target.split('.')
+    if (parts.length > 2) {
+        return parts[0]+'.'+parts.slice(1).join('')
+    }
+    return parts[0]
+}
+
+const EntryBox: React.FC<EntryBoxProps> = ({id, parent_class, label}) => {
     return (
         <div className={parent_class}>
             <span>{label}</span>
             <input
                 id={id}
                 className='Input'
-                type={type} // Allows for characters to be limited
-                step='.01'
-                onFocus={(e) => e.target.addEventListener("wheel", function (e) { e.preventDefault() }, { passive: false })} // Prevents scroll step                
+                // Makes it so that input boxes only allow numbers
+                onChange={(e) => e.target.value = /^\d*\.?\d*$/.test(e.target.value) ? e.target.value : (numerise(e.target.value.replace(/[^0-9.]/g, ''))) }
             />
         </div>
     )
